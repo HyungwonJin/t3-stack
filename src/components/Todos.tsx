c6f2acf659930e9a7493fa3faa8c2f5526aec87d
HyungwonJin <hwjin92@gmail.com>
2023-05-08 20:58:16 +0900

    create Todos component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,16 +6,19 @@ export function Todos() {
 
   if(isLoading) {
     return (
-      <div>
-        <div></div>
-        <p>loading...</p>
+      <div className="flex items-center justify-center">
+        <div
+          style={{borderTopColor: "transparent"}}
+          className="border-blue-200 mt-32 h-10 w-10 animate-spin rounded-full border-4"
+        />
+        <p className="mt-32 ml-4 text-xl">loading...</p>
       </div>
     )
   }
   if(isError){
     return (
-      <div>
-        <p>Error fetching todos</p>
+      <div className="flex items-center justify-center">
+        <p className="mt-10 ml-4 text-xl">Error fetching todos</p>
       </div>
     )
   }
@@ -24,7 +27,7 @@ export function Todos() {
     <>
       {todos.map((todo) => {
         return (
-          <section key={todo.id} >
+          <section key={todo.id} className="mt-8 space-y-4">
             <Todo todo={todo} />
           </section>
         )
